73fed18ff8eadf49251ca08205047b9b433a33ee
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-11-01 21:54:39 -0500

    pushing to GIt

---
+++ SpringClean/src/main/java/beans/favoriteThings.java
@@ -0,0 +1,23 @@
+/**
+ * @author Delanie Johnson - dmjohnson33
+ * CIS175 - Fall 2021
+ * Nov 1, 2021
+ */
+package beans;
+
+import lombok.Data;
+
+/**
+ * @author delan
+ *
+ */
+
+@Data
+public class favoriteThings {
+
+	private String color;
+	private String food;
+	private String music;
+	
+	
+}
---
+++ SpringClean/src/main/java/contoller/controller.java
@@ -0,0 +1,37 @@
+/**
+ * @author Delanie Johnson - dmjohnson33
+ * CIS175 - Fall 2021
+ * Nov 1, 2021
+ */
+package contoller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import beans.favoriteThings;
+
+
+/**
+ * @author delan
+ *
+ */
+@Controller
+public class controller {
+	
+	@GetMapping("/playagame")
+	
+	public String makeList(@RequestParam(name="choices",required=false) String foodChoice, String musicChoice, String colorChoice, Model model) {
+		if(colorChoice.equals(null) || foodChoice.equals(null) || musicChoice.equals(null)){
+			return "index";
+		}
+		
+		favoriteThings theList = new favoriteThings(colorChoice, foodChoice, musicChoice);
+		
+		model.addAttribute("favorite things", theList);
+		
+		return "results";
+	}
+
+}
+
